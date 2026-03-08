87cc31e675bbbf66f344ce8c6dff9c9218890809
Ibrahim Bello <ibrbello10@gmail.com>
2026-03-08 16:27:22 -0700

    10-minute update

---
+++ main.cpp
@@ -0,0 +1,40 @@
+// COMSC-210 | Lab 18 | Ibrahim Bello
+// This code asks the user stores movie reviews in a linked list.
+// The program asks the user whether they want the new reviews added at the beginning or end of the list
+// Once all reviews have been added, the program outputs the reviews and calculates the average rating.
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// Step 1: Node struct
+struct Node {
+    // data
+    double rating;
+
+};
+
+// Step 2: function prototypes 
+
+
+// Step 3: Main function
+int main() {
+    // Ask whether the user wants to add node to head or tail
+
+    // enter do-while loop
+    // if statement based on above, 
+
+    // if add to head: code below
+
+    // if add to tail: code below
+
+    // traverse list
+
+    // output total
+
+    // compute average
+
+    // print average
+
+    return 0;
+}

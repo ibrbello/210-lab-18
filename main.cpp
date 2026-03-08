07eb09f7459bb41199b39d96e6ec591cbdcb2947
Ibrahim Bello <ibrbello10@gmail.com>
2026-03-08 16:46:09 -0700

    10 minute update

--- main.cpp
+++ main.cpp
@@ -11,30 +11,42 @@ using namespace std;
 struct Node {
     // data
     double rating;
-
+    string comments;
+    // pointer
+    Node *next;
 };
 
-// Step 2: function prototypes 
-
-
-// Step 3: Main function
+// Step 2: Main function
 int main() {
     // Ask whether the user wants to add node to head or tail
+    cout << "Which linked list method should we use?" << endl;
+    cout << "   [1]: New nodes are added at the head of the linked list" << endl;
+    cout << "   [2]: New nodes are added at the tail of the linked list" << endl;
+    int methodChoice;
+    cin >> methodChoice;
+    cout << "The choice was " << methodChoice;
 
+    string anotherEntry;
     // enter do-while loop
+    do {
     // if statement based on above, 
+        if (methodChoice == 1) { // adding to head
 
-    // if add to head: code below
+        } 
 
-    // if add to tail: code below
+        else { // adding to tail
 
-    // traverse list
+        }
+        cout << "Do you want to enter another review? y/n: " << endl;
+        cin >> anotherEntry;
+    } while (anotherEntry != "n");
+   
+ // traverse list
 
     // output total
 
     // compute average
 
     // print average
-
     return 0;
 }
